8cc6a88d6255e8639b0dc6fdb0b7e6313309781a
Grayson Miller <grayson.miller124@gmail.com>
2021-12-02 21:37:30 -0700

    Added functional composition.
    Somewhat broken as it is not equivalent to single function passing because A parameterized partial doesn't pass it's parameters up properly.

--- functional.py
+++ functional.py
@@ -25,7 +25,16 @@ class Partial:
     def __repr__(self):
         return f'{self.f.__name__}({self.args}, {self.kwargs})'
 
+    def __add__(self, other):
+        if isinstance(other, Partial) and len(self.args):
+            forward = self.args[-1]
+            self.args = self.args[:-1]
+            return self(other(forward))
+        return self(other)
 
+                    
+            
+            
 def partial_map(f, it):
     for e in it:
         yield Partial(f, e)
@@ -39,32 +48,48 @@ def partial(*pargs, **pkwargs):
         return wrapper()
     return decorator
 
-
-def partial_test():    
+class Test:
+    # TODO: unittest / assert
+    @staticmethod
     def mysum(a, b, kw=None):
-        return a + b
+        return int.__add__(a, b)
 
     @partial(3)
+    @staticmethod
     def psum(a, b, kw=None):
-        return a + b
-
-    a = Partial(mysum)
-    print('a', a)
-    print('a()', a())
-    print('a(2)', a(2))
-    print('a(2)(2)', a(2)(2))
-    print('a(2, 2)', a(2, 2))
+        return int.__add__(a, b)
     
-    e = psum
-    print('e', e)
-    print('e()', e())
-    print('e(1)', e(1))
+    @staticmethod
+    def partial_test():    
+        a = Partial(Test.mysum)
+        print('a', a)
+        print('a()', a())
+        print('a(2)', a(2))
+        print('a(2)(2)', a(2)(2))
+        print('a(2, 2)', a(2, 2))
+        
+        e = Test.psum
+        print('e', e)
+        print('e()', e())
+        print('e(1)', e(1))
 
+    @staticmethod
+    def partial_map_test():
+        a = partial_map(lambda x, y, z: x - y + z, range(10, 1, -1))
+        print([pe(i)()(1) for i, pe in enumerate(a)])
 
-def partial_map_test():
-    x = partial_map(lambda x, y, z: x - y + z, range(10, 1, -1))
-    print([pe(i)()(1) for i, pe in enumerate(x)])
+    @staticmethod
+    def partial_comp_test():
+        # todo: fix + overloading such that partial doesn't interfere with result
+        a = Partial(Test.mysum)
+        b = Test.psum
+        c = a + b
+        d = a + b(1)
+        e = a(1) + b(1)
+        f = a(1) + b
+        print(a, b, c(1, 2), d(1), e, f(1, 2, 3), sep='\n')
 
-partial_test()
-partial_map_test()
+#Test.partial_test()
+#Test.partial_map_test()
+Test.partial_comp_test()
    
